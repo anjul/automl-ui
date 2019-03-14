261f6171e9724ed831b2a29b16a75cda44a270fa
Anjul Sharma <anjulnwo@gmail.com>
2019-03-15 00:18:56 +0800

    First Check-in

---
+++ webpack/webpack.dev.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+var parentDir = path.join(__dirname, '../src/');
+module.exports = {
+    entry: [
+        path.join(parentDir, 'index.js')
+    ],
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },{
+                test: /\.less$/,
+                loaders: ["style-loader", "css-loder", "less-loader"]
+            }
+        ]
+    },
+    output: {
+        path: parentDir + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: parentDir,
+        historyApiFallback: true
+    }
+}
