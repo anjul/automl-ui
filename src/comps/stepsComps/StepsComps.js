336a25be8838f50c35f6b6c5374aab05f5ea481b
anjul sharma <anjulnwo@gmail.com>
2019-03-21 21:05:27 +0800

    Commiting working code with bootstrap layouting enabled

--- src/comps/stepsComps/StepsComps.js
+++ src/comps/stepsComps/StepsComps.js
@@ -4,13 +4,11 @@ import { withStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import EditIcon  from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
-import BackgroundCard from '../BackgroundCard.js';
 import './StepsComps.css';
-import { CardContent } from '@material-ui/core';
 
 const styles = theme => ({
     root:{
-        flex: 2,
+        flex: 8,
     },
     fab: {
         margin: theme.spacing.unit,
@@ -24,18 +22,20 @@ function StepsComps(props) {
     const {classes} = props;
 
     return(
-        <div>
-            <BackgroundCard>
-                <CardContent>
-                    <b>{props.label}:</b>
-                    <Fab size="small" color="primary" aria-label="Edit" className={classes.fab} onClick={onEditBtn}>
-                        <EditIcon />
-                    </Fab>
-                    <Fab size="small" color="secondary" aria-label="Delete" className={classes.fab} onClick={onDelBtn}>
-                        <DeleteIcon />
-                    </Fab>
-                </CardContent>
-            </BackgroundCard>
+        <div className="stepDiv row">
+            
+            <div className="pull-left"><b>{props.label}:**</b></div>
+
+            <div className="pull-right"><Fab size="small" color="primary" aria-label="Edit" 
+            className={classes.fab} onClick={onEditBtn}>
+                <EditIcon />
+            </Fab>
+
+            <Fab size="small" color="secondary" aria-label="Delete" 
+            className={classes.fab} onClick={onDelBtn}>
+                <DeleteIcon />
+            </Fab></div>
+
         </div>
     );
 }
--- src/screens/CreateExperiment/CreateExperiment.js
+++ src/screens/CreateExperiment/CreateExperiment.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 import './CreateExperiment.css'
-import List from '@material-ui/core/List';
 import StepsComps from '../../comps/stepsComps/StepsComps';
 
 class CreateExperiment extends Component{
+    
     render(){
+              
+        const arrExperimentSteps = ["Select Variables","Explore Variables","Select Transformation","Configure Test & Validation"];
+        const steps = [];
+
+        for(const[index,value] of arrExperimentSteps.entries()){
+            steps.push(<StepsComps key={index} label={value}></StepsComps>)
+        }
+
         return(
-            <div className="stepsCompsDiv">
-                <StepsComps id="stepsComps" label="Select Variables" />
-                <StepsComps id="stepsComps" label="Explore Variables"/>
-                <StepsComps id="stepsComps" label="Select Transformation"/>
-                <StepsComps id="stepsComps" label="Configure Test & Validation"/>
-            </div>
-            
+            <div id="stepsDiv" >
+               {steps}
+            </div>    
         );
     }
 }
